a42ef0a2f0ffb4d55245690a156d5f4b7d88cb92
yjcho <jinjanic91@gmail.com>
2020-11-20 17:55:11 +0900

    프로그래머스 여행경로

--- Search/programmers_flightCourse.py
+++ Search/programmers_flightCourse.py
@@ -8,38 +8,41 @@
 
 # tickets = [["ICN", "A"], ["A", "C"], ["A", "D"], ["D", "B"], ["B", "A"]]
 
-tickets = [["ICN", "A"], ["ICN", "A"], ["A", "ICN"]]
+# tickets = [["ICN", "A"], ["ICN", "A"], ["A", "ICN"]]
+
+tickets = [["ICN", "COO"], ["ICN", "BOO"], ["COO", "ICN"], ["BOO", "DOO"]]
 
 
 def solution(tickets):
-    answer = ['ICN']  # 무조건 인천에서 출발
-    temp = []  # 재정렬 위한 임시배열
-    found = False
-    # now = "ICN"  # 항상 출발은 인천
-    while tickets:
-        for i in range(len(tickets)):
-            if answer[-1] == tickets[i][0]:
-                temp.append(tickets[i])
-        temp.sort()
-        print("temp의 상태", temp)
-        if len(temp) == 1:
-            answer.append(temp[0][1])
-            tickets.remove(temp[0])
+    tickets.sort(reverse=True)
+    routes = dict()
+    for t1, t2 in tickets:
+        # print("start, end", t1, t2)
+        if t1 in routes:
+            routes[t1].append(t2)
         else:
-            for i in range(len(temp)):
-                for j in range(len(tickets)):
-                    print('검사', temp[i][1], tickets[j][0])
-                    if temp[i][1] == tickets[j][0]:
-                        found = True
-                        break
-                if found:
-                    answer.append(temp[i][1])
-                    tickets.remove(temp[i])
-                    break
-        temp = []
-        print("지금 남은 티켓", tickets)
-        print("정답", answer)
+            routes[t1] = [t2]
+        # print('routes', routes)
 
+    start = ['ICN']
+    answer = []
+
+    while start:
+        # print('while start')
+        top = start[-1]
+        # print('top이', top, '여기 routes에 있니?', routes)
+        if top not in routes or len(routes[top]) == 0:
+            # print('루트에 업슴')
+            answer.append(start.pop())
+            # print("바뀐 answer", answer)
+            # print('바뀐 start', start)
+        else:
+            # print('루트에 존재')
+            start.append(routes[top].pop())
+            # print('바뀐 start', start)
+            # print('바뀐 routes', routes)
+    answer.reverse()
+    # print(answer)
     return answer
 
 
---
+++ Search/programmers_flightCourseFailed.py
@@ -0,0 +1,50 @@
+# tickets = [["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
+# return : [ICN, JFK, HND, IAD]
+
+# tickets = [["ICN", "SFO"], ["ICN", "ATL"], [
+#     "SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]
+
+# tickets = [["ICN", "A"], ["ICN", "B"], ["B", "ICN"]]
+
+# tickets = [["ICN", "A"], ["A", "C"], ["A", "D"], ["D", "B"], ["B", "A"]]
+
+# tickets = [["ICN", "A"], ["ICN", "A"], ["A", "ICN"]]
+
+tickets = [["ICN", "COO"], ["ICN", "BOO"], ["COO", "ICN"], ["BOO", "DOO"]]
+
+
+def solution(tickets):
+    answer = ['ICN']  # 무조건 인천에서 출발
+    temp = []  # 재정렬 위한 임시배열
+    found = False
+    # now = "ICN"  # 항상 출발은 인천
+    while len(tickets):
+        for i in range(len(tickets)):
+            if answer[-1] == tickets[i][0]:
+                temp.append(tickets[i])
+        temp.sort()
+        print("temp의 상태", temp)
+        if len(temp) == 1:
+            answer.append(temp[0][1])
+            tickets.remove(temp[0])
+        else:
+            if len(temp) == 0:
+                break
+            for i in range(len(temp)):
+                for j in range(len(tickets)):
+                    print('검사', temp[i][1], tickets[j][0])
+                    if temp[i][1] == tickets[j][0]:
+                        found = True
+                        break
+                if found:
+                    answer.append(temp[i][1])
+                    tickets.remove(temp[i])
+                    break
+        temp = []
+        print("지금 남은 티켓", tickets)
+        print("정답", answer)
+
+    return answer
+
+
+solution(tickets)
